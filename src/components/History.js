2ccaae6a570b214f2aece95cc32e7f232d579331
inezabonte <inezabonte@gmail.com>
2021-09-29 23:39:44 +0200

    fixing issue

--- src/components/History.js
+++ src/components/History.js
@@ -8,7 +8,7 @@ export default function History({ user }) {
 		return data;
 	};
 
-	const { data, isLoading } = useQuery(
+	const { data } = useQuery(
 		[user ? `travel-history/${user.email}` : null],
 		fetchHistory,
 		{ refetchInterval: 5000 }
@@ -17,13 +17,9 @@ export default function History({ user }) {
 	return (
 		<section className=" w-full max-w-md p-4 rounded">
 			<h2 className="font-bold text-xl text-purple-900 mb-4 ">History</h2>
-			<div className="space-y-4 divide-y-2">
-				{!user || isLoading ? (
-					<span className="text-purple-700 text-sm">
-						Travel history not available
-					</span>
-				) : (
-					data.history.map((item) => (
+			{data && (
+				<div className="space-y-4 divide-y-2">
+					{data.history.map((item) => (
 						<div
 							key={item.id}
 							className="text-purple-900 flex justify-between  p-2"
@@ -31,9 +27,9 @@ export default function History({ user }) {
 							<span>📍{item.fields.location}</span>
 							<span>{item.fields.year}</span>
 						</div>
-					))
-				)}
-			</div>
+					))}
+				</div>
+			)}
 		</section>
 	);
 }
