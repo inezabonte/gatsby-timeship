58a7ac5294594771e95b6fd339288773aceb67bc
inezabonte <inezabonte@gmail.com>
2021-11-15 14:19:52 +0200

    add snow

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,11 @@ module.exports = {
 		siteUrl: "https://www.yourdomain.tld",
 		title: "Gastby Timeship",
 	},
-	plugins: ["gatsby-plugin-postcss", "gatsby-plugin-gatsby-cloud"],
+	plugins: ["gatsby-plugin-postcss", "gatsby-plugin-gatsby-cloud",
+	{
+		resolve: "@raae/gatsby-plugin-let-it-snow",
+		options: {
+			colors:["#161032", "#5BC0EB", "#CA3C25", "#9BC53D"]
+		}
+	}],
 };
